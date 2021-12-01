a6d07d0937a49803d643038265b3fccb10ecc298
Ilenia Castellana <ilenia.castellana@gmail.com>
2021-12-01 17:35:45 +0100

    add utils file

---
+++ js/utils.js
@@ -0,0 +1,21 @@
+
+export const bearToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDQyMDRlZWVjZWFiOWYwM2ZiYjhjZDg2MDA4NWVhMCIsInN1YiI6IjYxYTRhMjFjNGE1MmY4MDA0NDY5NmU3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EhSsODDAPV564ugGStjgFPSg2sdolrSU42JgJXuCpzI';
+export const APIendpoint = 'https://api.themoviedb.org/3';
+
+export const render = (container, content) => (container.innerHTML = content);
+
+
+// Docs 
+// Images
+// https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400
+// https://www.themoviedb.org/talk/5abcef779251411e97025408
+// Videos
+// https://www.themoviedb.org/talk/5955219ec3a3680d73048c7e
+
+// TODO
+// add device handling - image, content...
+// add local storage for last card selected when user navigates back to the home page
+// add more details in Movie Details page
+// add filtering by genre ? 
+// add language setting
+// add searching by name ?
