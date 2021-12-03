1c252a6cb82cdd6d2061a833f52950a827753098
Ilenia Castellana <ilenia.castellana@gmail.com>
2021-12-03 10:52:44 +0100

    add responsive preview

--- js/preview.js
+++ js/preview.js
@@ -2,16 +2,22 @@ import { render } from './utils.js';
 
 const Preview = (data, id) => {
   const item = data.filter((card) => card.id == id)
+  const mobilePreview = `https://image.tmdb.org/t/p/original/${item[0].poster_path}`;
+  const desktopPreview = `https://image.tmdb.org/t/p/original/${item[0].backdrop_path}`;
 
   const heroElement =
   `
-    <div class="hero-bg" style="background-image: url('https://image.tmdb.org/t/p/original/${item[0].backdrop_path}');">
-        <div class="hero-details">
-            <h2>${item[0].name}</h2>
-            <p>${item[0].overview.length > 600 ? item[0].overview.substring(0,600)+"..." : item[0].overview }</p>
-            <button class="custom-btn" id=${item[0].id}>Details</a>
-        </div>
+    <div class="hero-bg">
+      <picture>
+          <source media="(min-width: 991px)" srcset="${desktopPreview}">
+          <img src="${mobilePreview}">
+      </picture>
     </div>
+      <div class="hero-details">
+          <h2>${item[0].name}</h2>
+          <p class=${item[0].overview.length > 550 ? "truncate" : null} >${item[0].overview}</p>
+          <button class="custom-btn" id=${item[0].id}>Details</a>
+      </div>
   `;
 
   const hero = document.querySelector(".container-hero");
