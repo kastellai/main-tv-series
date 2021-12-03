00060769b5080c4abd81871195a302c8a714e0b1
Ilenia Castellana <ilenia.castellana@gmail.com>
2021-12-03 13:14:31 +0100

    add image placeholders add locat storage handling for page and card fix preview flickering

--- js/index.js
+++ js/index.js
@@ -1,30 +1,37 @@
 import { List } from './list.js';
 import { Preview } from './preview.js';
 
-import { renderWelcome } from './preview.js';
+import { savePageLS, getPageLS, getCardLS, saveCardLS } from './locaStorage.js';
+// import { renderWelcome } from './preview.js';
 import { fetchMoviesList } from './tmdbapi.js';
 
 export let data = [];
 
 const renderMoviesList = (data) => {
     List(data);
+
     let cards = document.querySelectorAll('.card');
     cards.forEach((card) => {
         card.addEventListener("click", (e) => {
-            Preview(data, e.currentTarget.id);
+            const item = data.filter((card) => card.id == e.currentTarget.id);
+            saveCardLS((JSON.stringify(item[0])));
+            Preview(JSON.parse(getCardLS()));
             location.href='#preview';
         });
     });
 };
 
 const renderCards = () => {
-    fetchMoviesList(currentPage).then((result) => {
+    fetchMoviesList(getPageLS()).then((result) => {
         data = result; 
         renderMoviesList(result); });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     // renderWelcome();
+    if (getCardLS()) {
+        Preview(JSON.parse(getCardLS()));
+    }
     renderCards();   
 });
 
@@ -33,11 +40,17 @@ const prev = document.querySelector('.arrow.lft');
 let currentPage = 1;
 
 next.addEventListener('click', () => {
+    getPageLS() ? currentPage = getPageLS(): null;
     currentPage++; 
+    savePageLS(currentPage);
     renderCards();
 });
 
 prev.addEventListener('click', () => {
-    currentPage > 1 ? currentPage-- : null; 
-    renderCards();
+    currentPage = getPageLS();
+    if (currentPage > 1) {
+        currentPage--;
+        savePageLS(currentPage);
+        renderCards();
+    }
 });
--- js/preview.js
+++ js/preview.js
@@ -1,9 +1,9 @@
 import { render } from './utils.js';
 
-const Preview = (data, id) => {
-  const item = data.filter((card) => card.id == id)
-  const mobilePreview = `https://image.tmdb.org/t/p/original/${item[0].poster_path}`;
-  const desktopPreview = `https://image.tmdb.org/t/p/original/${item[0].backdrop_path}`;
+const Preview = (record) => {
+
+  const mobilePreview = record.poster_path ? `https://image.tmdb.org/t/p/original/${record.poster_path}` : `./assets/404-error-page-not-found-m.jpeg`;
+  const desktopPreview = record.backdrop_path ? `https://image.tmdb.org/t/p/original/${record.backdrop_path}` : `./assets/404-error-page-not-found.jpeg`;
 
   const heroElement =
   `
@@ -14,9 +14,9 @@ const Preview = (data, id) => {
       </picture>
     </div>
       <div class="hero-details">
-          <h2>${item[0].name}</h2>
-          <p class=${item[0].overview.length > 550 ? "truncate" : null} >${item[0].overview}</p>
-          <button class="custom-btn" id=${item[0].id}>Details</a>
+          <h2>${record.name}</h2>
+          <p class=${record.overview.length > 550 ? "truncate" : null} >${record.overview}</p>
+          <button class="custom-btn" id=${record.id}>Details</a>
       </div>
   `;
 
@@ -26,7 +26,7 @@ const Preview = (data, id) => {
   let detailsBtn = document.querySelectorAll('.custom-btn');
   detailsBtn.forEach((btn) => {
       btn.addEventListener("click", (e) => {
-          location.href = `./tv-series.html?id=${e.currentTarget.id}`;
+        location.href = `./tv-series.html?id=${e.currentTarget.id}`;
       });
   });
 }
